4e4053e22a6466b4a89768213a73e7451fa8e22d
IK <ingkhing05@hotmail.com>
2024-11-04 21:45:37 +0800

    add testing git hub file

---
+++ src/topic_2_ledger_system/testingGitHubChangingValueInMethodAndOtherRandomTest.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package topic_2_ledger_system;
+
+/**
+ *
+ * @author Wong Ing Khing
+ * uhhhhh this file is for testing how the github push and pull works
+ * 
+ */
+public class testingGitHubChangingValueInMethodAndOtherRandomTest {
+    
+}

835c3234c299832cbbc6c83217fd2dbd1b8da019
Spjsss <peijunsim08@gmail.com>
2024-11-07 13:04:17 +0800

    update confirmation password and password validity checking function

--- src/topic_2_ledger_system/login_register.java
+++ src/topic_2_ledger_system/login_register.java
@@ -45,12 +45,65 @@ public class login_register {
         System.out.println("Password: ");
         String password = input.nextLine();
         
+        boolean passwordValid = passwordValidity(password);
+        while (passwordValid == false){
+            invalidPassword(password);
+            passwordValid = passwordValidity(password);
+        }    
+        
         System.out.println("Confirmation password: ");
         String confirmationPassword = input.nextLine();
         
         boolean passwordMatch = comfirmPassword(password, confirmationPassword);
-        if (passwordMatch == false)
-            password = incorrectPassword(password, confirmationPassword);
+        if (passwordMatch == false){
+            password = unmatchedPassword(password, confirmationPassword);
+        }
+    }
+    
+    private static boolean passwordValidity(String password) {
+        char pwdCheck;
+        int upperCaseCount = 0, lowerCaseCount = 0, numCount = 0,specialCharCount = 0;
+        int minLength = 8;
+        
+            
+        for (int i = 0; i< password.length(); ++i){
+                
+            pwdCheck = password.charAt(i);
+                
+            if ( pwdCheck >= 'A' && pwdCheck <= 'Z'){
+                ++ upperCaseCount;
+            }
+                
+            else if ( pwdCheck >= 'a' && pwdCheck <= 'z'){
+                    ++ lowerCaseCount;
+            }
+                
+            else if ( pwdCheck >= '0' && pwdCheck <= '9'){
+                    ++ numCount;
+            }
+                
+            else if ( (pwdCheck >= '!' && pwdCheck <= '~')){
+                    ++specialCharCount;
+            }
+            }
+        
+        
+        if (password.length() >= minLength && upperCaseCount >= 1 && lowerCaseCount >=1 && numCount >= 1 && specialCharCount >=1){
+            return true;
+        }
+        else{
+            return false;
+                    
+        } 
+        
+    }
+    
+    private static String invalidPassword(String password){
+        Scanner input = new Scanner (System.in);
+        System.out.println("Invalid Password");
+        System.out.print("Password: ");
+        password = input.nextLine();
+        return password;
     }
     
     private static boolean comfirmPassword(String password, String confirmationPassword) {
@@ -61,20 +114,26 @@ public class login_register {
         }
     }
     
-    private static String incorrectPassword(String password, String confirmationPassword) {
+    private static String unmatchedPassword(String password, String confirmationPassword) {
         Scanner input = new Scanner(System.in);
-        while (!password.equals(confirmationPassword)) {
+        while (!password.equals(confirmationPassword)){
             System.out.println("Password didn't match");
             System.out.println("Password: ");
+            boolean passwordValid = passwordValidity(password);
+            while (passwordValid == false){
+                password = invalidPassword(password);
+                passwordValid = passwordValidity(password);
+            }
+        
             password = input.nextLine();
         
             System.out.println("Confirmation password: ");
             confirmationPassword = input.nextLine();
-            
-                
         }
-        return password;
+            return password;
     }
+    
+ 
     
     private boolean validateEmail(String e_mail) {
         

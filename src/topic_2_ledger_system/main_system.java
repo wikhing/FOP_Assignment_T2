fb7273ca3b6d85aa3738fdd215609df90b919284
Harry <Harry@LAPTOP-MIH9K9NS>
2024-11-26 16:44:04 +0800

    Transaction History and export CSV function completed The issue in calling user_id also fixed

--- src/topic_2_ledger_system/main_system.java
+++ src/topic_2_ledger_system/main_system.java
@@ -23,6 +23,19 @@ public class main_system {
      */
     public static void main(String[] args) throws IOException{
         login_register.initialize();
+        
+        //Call the method for Transaction History
+        try {
+            // Initialize login or registration process
+            int user_id = login_register.initialize();
 
+            // Get the month and year from the user
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter the month and year (MM-YYYY): ");
+            String monthYear = scanner.nextLine();
+            view_export_csv.viewAndExportTransactions(user_id, monthYear);
+        } catch (IOException e) {
+            System.err.println("An error occurred: " + e.getMessage());
+        }
     }
 }

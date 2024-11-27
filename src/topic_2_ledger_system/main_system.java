93392ac4f4268f1a53b677f59ff0a32860678c6e
Teo <teoyk9810@gmail.com>
2024-11-27 15:22:29 +0800

    Test

--- src/topic_2_ledger_system/main_system.java
+++ src/topic_2_ledger_system/main_system.java
@@ -21,13 +21,50 @@ public class main_system {
     /**
      * @param args the command line arguments
      */
+    
+    
+    
+    public static void printMenu(String username, double balance, double saving, double loan) {
+            System.out.println("\n\n== Welcome, " + username + " ==");
+            System.out.print("Balance: " );
+            System.out.printf("%.2f",balance);
+            System.out.print("\nSavings: ");
+            System.out.printf("%.2f", saving);
+            System.out.print("\nLoan: ");
+            System.out.printf("%.2f",loan);
+
+            System.out.print("\n\n== Transaction ==");
+            System.out.print("\n1. Debit");
+            System.out.print("\n2. Credit");
+            System.out.print("\n3. History");
+            System.out.print("\n4. Savings");
+            System.out.print("\n5. Credit Loan");
+            System.out.print("\n6. Deposit Interest Predictor");
+            System.out.print("\n7. Logout");
+            System.out.print("\n>");
+    }
+    
+    
+    public static void loginPage(int user_id) {
+        ArrayList<String[]> user_csv = file.get_user_csv();
+        String username = user_csv.get(user_id)[1];
+        String email = user_csv.get(user_id)[2];
+        
+        printMenu(username, file.get_accbalance_csv(user_id), get_saving, get_loan)
+    }
+    
+    
     public static void main(String[] args) throws IOException{
-        login_register.initialize();
+        
+        int user_id = login_register.initialize();
+        
+        loginPage(user_id);
+        
         
         //Call the method for Transaction History
         try {
             // Initialize login or registration process
-            int user_id = login_register.initialize();
+            user_id = login_register.initialize();
 
             // Get the month and year from the user
             Scanner scanner = new Scanner(System.in);

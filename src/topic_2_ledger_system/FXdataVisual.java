641825e89bd7e5ea4ac7ed830717383709c0e491
IK <ingkhing05@hotmail.com>
2024-12-30 11:25:38 +0800

    test pie chart

--- src/topic_2_ledger_system/FXdataVisual.java
+++ src/topic_2_ledger_system/FXdataVisual.java
@@ -38,6 +38,7 @@ import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.renderer.category.LineAndShapeRenderer;
 import org.jfree.chart.ui.ApplicationFrame;
 import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
 
 /**
  *
@@ -143,7 +144,7 @@ public class FXdataVisual extends JFrame{
     
     public void displaySpendingTrends(int user_id, int month, int year) {
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-//        PieDataSet dataset = new PieDataSet();
+//        DefaultPieDataset pieDataset = new DefaultPieDataset();
         
         List<String> xAxisMonthYear = getXAxisMonthYear (month, year);
         List<Double> monthlyDebit = getDebitfor12Months(user_id, month, year);
@@ -151,9 +152,12 @@ public class FXdataVisual extends JFrame{
         
         for (int i = 0; i < monthlyDebit.size(); i++) {
             dataset.addValue(monthlyDebit.get(i), "Spending", xAxisMonthYear.get(i));
+//            pieDataset.setValue("Test1", Double.valueOf(20));
+//            pieDataset.setValue("Test2", Double.valueOf(40));
+//            pieDataset.setValue("Test3", Double.valueOf(10));
         }
         
-//        JFreeChart pieChart = ChartFactory.createPieChart("Spending Trends in 12 Months", dataset, true, true, false);
+//        JFreeChart pieChart = ChartFactory.createPieChart("Spending Trends in 12 Months", pieDataset, true, true, false);
         JFreeChart barChart = ChartFactory.createBarChart(
                 "Spending Trends Over Time",   // Chart title
                 "Month",                       // X-axis label
@@ -165,6 +169,8 @@ public class FXdataVisual extends JFrame{
                 false                          // URLs
         );
 
+//        ChartPanel pieChartPanel = new ChartPanel(pieChart);
+//        pieChartPanel.setPreferredSize(new java.awt.Dimension(1200, 1000));
 
         ChartPanel chartPanel = new ChartPanel(barChart);
         chartPanel.setPreferredSize(new java.awt.Dimension(1200, 1000));
